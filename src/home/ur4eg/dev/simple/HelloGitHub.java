1fd9b61aa7901fc6ea01b46b53d27cc6ce0ff5c0
Ur4eG <Ur4eG@icloud.com>
2016-01-28 22:58:34 +0200

    Initial commit

---
+++ src/home/ur4eg/dev/simple/HelloGitHub.java
@@ -0,0 +1,11 @@
+package home.ur4eg.dev.simple;
+
+/**
+ * тестовая проргамма для того чтобы посмотреть, как
+ * выгрузятся на GitHub ТОЛЬКО файлы относящиеся к программе(не проекта)
+ */
+public class HelloGitHub {
+    public static void main(String[] args) {
+        System.out.println("Hello github");
+    }
+}
